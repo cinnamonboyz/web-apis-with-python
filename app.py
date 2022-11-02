648f2980c932ee07ba6e924bce9d240b85d21900
cinnamonboyz <18220012@std.stei.itb.ac.id>
2022-11-02 19:41:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
+
+    # first_name = request.args.get('first_name')
+    # second_name = request.args.get('second_name')
+
+    # if first_name and second_name:
+    #     response = {"data": f"Is your name {first_name} {second_name}"}
+    # elif first_name:
+    #     response = {"data": f"Hello, {first_name}!"}
+    # elif second_name:
+    #     response = {"data": f"Hello Mr {second_name}!"}
+    # else:
+    #     response = {"status": "error"}
+
     response = {"data": "Hello, World!"}
     return jsonify(response)
